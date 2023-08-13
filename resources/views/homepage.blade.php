df9ce1cc764a7f706079e2e6d6cd5a650a578d43
Filip Ganyicz <ganyicz.filip@gmail.com>
2023-08-13 17:24:12 +0200

    Add layout

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,21 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-        <!-- Styles -->
-        <script src="https://cdn.tailwindcss.com"></script>
-    </head>
-    <body class="antialiased">
-        @foreach($groups as $group)
-            <a href="{{ route('group', $group) }}">{{ $group->name }}</a>
-        @endforeach
-    </body>
-</html>
+<x-layout>
+    @foreach($groups as $group)
+        <a href="{{ route('group', $group) }}">{{ $group->name }}</a>
+    @endforeach
+</x-layout>
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+    <body class="antialiased">
+        {{ $slot }}
+    </body>
+</html>
