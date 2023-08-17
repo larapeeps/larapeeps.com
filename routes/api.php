2489d82fe08d087e5c5d9dbdaff7c449631ead75
Steve McDougall <juststevemcd@gmail.com>
2023-08-17 12:15:03 +0100

    Adding API endpoints

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 declare(strict_types=1);
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Peeps\AvatarController;
+use App\Http\Controllers\Peeps\ShowController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user', fn (Request $request) => $request->user());
+Route::middleware(['cache.headers:public;max_age=2628000;etag'])->group(static function (): void {
+    Route::get('peeps/{slug}', ShowController::class)->name('peep:show');
+    Route::get('peeps/{slug}/avatar', AvatarController::class)->name('peep:avatar');
+});
