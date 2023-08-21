21da3f32cea1c47800cceb065da0ee2c53f97389
Filip Ganyicz <ganyicz.filip@gmail.com>
2023-08-21 15:02:45 +0200

    Update vite plugin

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            valetTls: 'larapeeps.test'
         }),
     ],
 });
